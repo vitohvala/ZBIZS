10f197e991a04c9984f939baa24179a32ad3e2e2
vitohvala <hvalavitalisu@gmail.com>
2023-06-07 18:10:59 +0200

    first

---
+++ ZastitaBZ/PromeniUC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ZastitaBZ
+{
+    public class PromeniUC
+    {
+        public static void promeniUC(UserControl novaUC, Panel panel)
+        {
+            panel.Controls.Clear();
+            panel.Controls.Add(novaUC);
+            novaUC.Dock = DockStyle.Fill;
+        }
+    }
+}
